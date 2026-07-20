a29c1ed5970c1aaddd35da724974714458c3a45c
Moritz Messner <moritzmessner122@gmail.com>
2026-07-20 22:59:37 +0200

    changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 import { defaultLang, locales } from './src/i18n/config.ts';
 import { createSitemapSerialize } from './src/i18n/sitemap.ts';
 
-const site = 'https://meeeeez.github.io';
-const base = '/tandem-v3';
+const site = 'https://airadventure.it';
+const base = '/';
 
 // https://astro.build/config
 export default defineConfig({
